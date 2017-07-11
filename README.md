bdd6276074f2e748daaa39dad35d46cd70ba1d3b
Anders K. Madsen <akm@novicell.dk>
2017-07-11 10:41:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Gut
+
+Gut is a minimal reimplementation of GNU `cut` that supports defining
+delimiters as regular expressions.
+
+## Command line options
+
+`-d/-delimiter REGEX` Regular expression to use for splitting input into
+fields. Default: `\s+`.
+
+`-f/-fields FIELDS` Collection of fields to output. Can be specified as a
+comma separated list of fields and ranges. (Note: Like `cut`, first field is 1,
+not 0.) Ranges can be defined as: `2-4`, `2:4`, `2..4` or `2...4`. Default:
+Output all fields.
+
+## Examples
+
+```
+$ echo "1,2, 3. 4,5" | gut -d "(,|.)\s?" -f 1,3-5
+1 3 4 5
+```
+
+## Todo
+
+ * Access fields by numbers relative to the last field.
+ * Crop field output to X bytes
+
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var delim string
+var delimDefault *regexp.Regexp = regexp.MustCompile(`\s+`)
+var field string
+var fieldDefault string = ""
+
+func makeRange(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
+
+func parseFieldString(param string) []int {
+	var fields []int
+
+	substr := strings.Split(param, `,`)
+
+	isRange := regexp.MustCompile(`^[0-9]+(-|\.{2,3}|:)[0-9]+$`)
+
+	for _, v := range substr {
+		if s, err := strconv.Atoi(v); err == nil {
+			fields = append(fields, s-1)
+			continue
+		}
+
+		if isRange.MatchString(v) {
+			subsubstr := strings.Split(v, `-`)
+			min, _ := strconv.Atoi(subsubstr[0])
+			max, _ := strconv.Atoi(subsubstr[1])
+			fields = append(fields, makeRange(min-1, max-1)...)
+		}
+	}
+
+	return fields
+}
+
+func init() {
+	flag.StringVar(&delim, `delimiter`, delimDefault.String(), `Regex to use as delimiter`)
+	flag.StringVar(&delim, `d`, delimDefault.String(), `Short form of -delimiter`)
+
+	flag.StringVar(&field, `fields`, fieldDefault, `Field(s) to display`)
+	flag.StringVar(&field, `f`, fieldDefault, `Short form of -fields`)
+
+	flag.Parse()
+}
+
+func main() {
+	re := regexp.MustCompile(delim)
+	fields := parseFieldString(field)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	var tmp []string
+	var out []string
+
+	for scanner.Scan() {
+		tmp = re.Split(scanner.Text(), -1)
+
+		if len(fields) == 0 {
+			fields = makeRange(0, len(tmp)-1)
+		}
+
+		for _, i := range fields {
+			// Does field i even exist?
+			if len(tmp) > i {
+				out = append(out, tmp[i])
+			}
+		}
+		fmt.Printf("%s\n", strings.Join(out, ` `))
+	}
+}
